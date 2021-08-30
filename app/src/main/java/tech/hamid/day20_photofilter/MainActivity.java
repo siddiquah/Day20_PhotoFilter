38fcbfd0e019b7ff434f77deec8beb86d891a17d
siddiquah anjum <siddiquahanjum111@gmail.com>
2021-08-30 17:03:04 +0530

    initial commit

---
+++ app/src/main/java/tech/hamid/day20_photofilter/MainActivity.java
@@ -0,0 +1,19 @@
+package tech.hamid.day20_photofilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
